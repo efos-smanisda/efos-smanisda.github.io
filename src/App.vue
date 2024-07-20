22d9b0650ec5734697acb1dbeec0d0a6d3317072
efos-smanisda <evanadhiarjayohanes1@gmail.com>
2024-07-20 13:16:53 +0700

    testing

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   import { ref } from 'vue';
   const menuActive = ref(false);
   function toggleMenu() {
-    this.menuActive = !menuActive.value;
+    menuActive.value = !menuActive.value;
   }
 </script>
 
@@ -46,7 +46,7 @@
 
     <!-- introduction -->
 
-    <section class="introduction" id="introduction">
+    <!-- <section class="introduction" id="introduction">
       <div class="max-width">
         <h2 class="title">Introduction me</h2>
         <div class="introduction-content">
@@ -60,13 +60,13 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
 
 
 
     <!-- projects -->
 
-    <section class="projects" id="projects">
+    <!-- <section class="projects" id="projects">
       <svg class="wave-up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="aliceblue" fill-opacity="1" d="M0,128L48,138.7C96,149,192,171,288,192C384,213,480,235,576,224C672,213,768,171,864,165.3C960,160,1056,192,1152,197.3C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
       <div class="max-width">
         <h2 class="title">My projects</h2>
@@ -95,11 +95,11 @@
         </div>
       </div>
       <svg class="wave-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f0f8ff" fill-opacity="1" d="M0,224L48,234.7C96,245,192,267,288,245.3C384,224,480,160,576,154.7C672,149,768,203,864,192C960,181,1056,107,1152,106.7C1248,107,1344,181,1392,218.7L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-    </section>
+    </section> -->
 
     <!-- skill -->
 
-    <section class="skills" id="skills">
+    <!-- <section class="skills" id="skills">
       <div class="max-width">
         <h2 class="title">My Skills</h2>
         <div class="skills-content">
@@ -132,11 +132,11 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
 
     <!-- contact -->
 
-    <section class="contact" id="contact">
+    <!-- <section class="contact" id="contact">
       <svg class="wave-up" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f0f8ff" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,229.3C384,213,480,171,576,170.7C672,171,768,213,864,213.3C960,213,1056,171,1152,133.3C1248,96,1344,64,1392,48L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
       <div class="max-width">
         <h2 class="title">Contact me</h2>
@@ -169,31 +169,11 @@
             </div>
           </div>
           <div class="column right">
-            <!--<div class="text">More contact.</div>
-            <div class="row-1 wrapper">
-              <div class="button">
-                <div class="icon"><i class="fab fa-discord"></i></div>
-                <span>Habaru#2007</span>
-              </div>
-              <div class="button">
-                <div class="icon"><i class="fab fa-instagram"></i></div>
-                <span>Instagram</span>
-              </div>
-            </div>
-            <div class="row-2 wrapper ">
-              <div class="button">
-                <div class="icon"><i class="fab fa-github"></i></div>
-                <span>Github</span>
-              </div>
-              <div class="button">
-                <div class="icon"><i class="fab fa-youtube"></i></div>
-                <span>Youtube</span>
-              </div>       
-            </div>-->
+            
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
 
     <footer>
       <span>Created By <a href="#">Evan Adhiarja Yohanes (Habaru)</a> with <span class="fas fa-heart"></span> | <span class="far fa-copyright"></span>2021 All rights reserved</span>
